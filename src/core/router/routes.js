e7b4e679e6e9bd924bf984a9b4e041fd619d1743
Andrés Rojas <eurekacloud2019@gmail.com>
2022-09-23 16:04:45 +0200

    remove src

--- src/core/router/routes.js
+++
@@ -1,6 +0,0 @@
-export const baseRoutes = {
-  home: '/pages/home/home.html',
-  answerList: '/pages/answer-list/answer-list.html',
-  generatedCode: '/pages/generated-code/generated-code.html'
-};
-
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import { history, baseRoutes } from './core/router';
-history.push(baseRoutes.home);
